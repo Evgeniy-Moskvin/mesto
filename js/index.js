2745a15d16a5cb587aa0ad40903640732f247ee7
Evgeniy Moskvin <moskwin.evgeny@gmail.com>
2023-01-30 00:33:47 +0300

    feat: реализовать рендер, лайки, удаление, просмотр изображения у карточек мест

--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@ let buttonClose = document.querySelector('.button-close');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
+const popupFullImage = document.querySelector('.popup_full-image');
+
+const placesCardList = document.querySelector('.places__grid');
+const placeCardTemplate = document.querySelector('#place-card-template').content;
+
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
@@ -32,3 +37,43 @@ function closePopup() {
 buttonEdit.addEventListener('click', editProfile);
 
 buttonClose.addEventListener('click', closePopup);
+
+// CARDS
+// =====
+
+function createPlaceCard(item) {
+  const card = placeCardTemplate.cloneNode(true);
+  card.querySelector('.place-card__name').textContent = item.name;
+  card.querySelector('.place-card__image').src = item.link;
+  card.querySelector('.place-card__image').alt = item.name;
+
+  card.querySelector('.place-card__like').addEventListener('click', doLike);
+  card.querySelector('.place-card__delete').addEventListener('click', deleteCard);
+  card.querySelector('.place-card__image').addEventListener('click', showImage);
+
+  return card;
+}
+
+function deleteCard(evt) {
+  evt.target.closest('.places__item').remove();
+}
+
+function doLike(evt) {
+  evt.target.classList.toggle('button-like_active');
+}
+
+function showImage(evt) {
+  popupFullImage.querySelector('.popup__image').src = evt.target.src;
+  popupFullImage.querySelector('.popup__image').alt = evt.target.alt;
+  popupFullImage.classList.add('popup_opened');
+}
+
+function renderPlaceCard(items) {
+  const cards = items.map((item) => {
+    return createPlaceCard(item);
+  });
+
+  placesCardList.append(...cards);
+}
+
+renderPlaceCard(initialCards);
