a9f2b1b48e7a0c68884117f3f2eee5c0763e6d64
Evgeniy-Moskvin <moskwin.evgeny@gmail.com>
2023-05-04 02:38:45 +0300

    refactor: реализовать класса UserInfo.js для редактирования профиля

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,9 @@ import Card from '../components/Card.js'
 import Section from '../components/Section.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
+import FormValidator from '../components/FormValidator.js';
+
 import {
   popupWithImageSelector,
   placesCardListSelector,
@@ -15,70 +18,19 @@ import {
   popupEditProfileSelector,
   buttonEditProfile,
   buttonAddPlaceCard,
+  formEditProfile,
+  formAddPlaceCard,
+  profileNameSelector,
+  profileJobSelector,
+  profileNameInput,
+  profileJobInput,
 } from '../utils/constants.js';
 
-//const popupEditProfile = document.querySelector('.popup_name_edit-profile');
-//const popupAddPlaceCard = document.querySelector('.popup_name_add-place-card');
-const popupFullImage = document.querySelector('.popup_name_full-image');
-const popupImageName = popupFullImage.querySelector('.popup__image-name');
-const popups = document.querySelectorAll('.popup');
-
-const formEditProfile = document.forms['form-edit-profile'];
-const profileNameInput = formEditProfile.querySelector('.form__input_name_name');
-const profileJobInput = formEditProfile.querySelector('.form__input_name_job');
-//const buttonEditProfile = document.querySelector('.button-edit');
-//const buttonAddPlaceCard = document.querySelector('.button-add');
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
-
-const formAddPlaceCard = document.forms['form-add-place-card'];
-const placeNameInput = formAddPlaceCard.querySelector('.form__input_name_name');
-const placeImageInput = formAddPlaceCard.querySelector('.form__input_name_image');
-const placeFullImage = popupFullImage.querySelector('.popup__image');
-
-
-import { FormValidator } from '../components/FormValidator.js';
-import { data } from 'autoprefixer';
-//import PopupWithFrom from '../components/PopupWithForm.js';
 
 const formEditProfileValidate = new FormValidator(config, formEditProfile);
 const formAddPlaceCardValidate = new FormValidator(config, formAddPlaceCard);
 
-// POPUPS
-// ======
-
-/*const openPopup = (popup) => {
-  popup.classList.add('popup_opened');
-
-  document.addEventListener('keydown', closePopupByEsc);
-}
-
-const closePopup = (popup) => {
-  popup.classList.remove('popup_opened');
-
-  document.removeEventListener('keydown', closePopupByEsc);
-}
-
-const closePopupByEsc = (evt) => {
-  if (evt.key === 'Escape') {
-    closePopup(document.querySelector('.popup_opened'));
-  }
-}*/
-
-/*popups.forEach((popupElement) => {
-  popupElement.addEventListener('mousedown', (evt) => {
-    if (evt.target.classList.contains('popup_opened')) {
-      closePopup(popupElement)
-    }
-    if (evt.target.classList.contains('popup__close')) {
-      closePopup(popupElement)
-    }
-  });
-});*/
-
-const addPlaceCard = ({ name, image: link }) => {
-  placeCard.addItem(createPlaceCard({ name, link }))
-}
+const userInfo = new UserInfo({ profileNameSelector, profileJobSelector });
 
 const popupWithImage = new PopupWithImage(popupWithImageSelector);
 popupWithImage.setEventListeners();
@@ -86,10 +38,18 @@ popupWithImage.setEventListeners();
 const popupAddPlaceCard = new PopupWithForm(popupAddPlaceCardSelector, addPlaceCard);
 popupAddPlaceCard.setEventListeners();
 
-const popupEditProfile = new PopupWithForm(popupEditProfileSelector, () => {
-
-});
+const popupEditProfile = new PopupWithForm(popupEditProfileSelector, editProfile);
 popupEditProfile.setEventListeners();
+
+
+const addPlaceCard = ({ name, image: link }) => {
+  placeCard.addItem(createPlaceCard({ name, link }))
+}
+
+const editProfile = ({ name, job }) => {
+  userInfo.setUserInfo({ name, job });
+}
+
 
 const handleCardClick = (src, name) => {
   popupWithImage.open(src, name);
@@ -101,41 +61,20 @@ buttonAddPlaceCard.addEventListener('click', () => {
 });
 
 buttonEditProfile.addEventListener('click', () => {
+  initProfileEditor();
   popupEditProfile.open();
 })
 
 
-// PROFILE
-// =======
-
 const initProfileEditor = () => {
-  profileNameInput.value = profileName.textContent;
-  profileJobInput.value = profileJob.textContent;
+  profileNameInput.value = userInfo.getUserInfo().name;
+  profileJobInput.value = userInfo.getUserInfo().job;
 }
 
-/*const editProfile = (evt) => {
-  evt.preventDefault();
-
-  profileName.textContent = profileNameInput.value;
-  profileJob.textContent = profileJobInput.value;
-
-  closePopup(popupEditProfile);
-}*/
-
-/*formEditProfile.addEventListener('submit', editProfile);*/
-
-/*buttonEditProfile.addEventListener('click', () => {
-  formEditProfileValidate.resetForm();
-  initProfileEditor();
-  openPopup(popupEditProfile);
-});*/
 
 initProfileEditor();
 formEditProfileValidate.enableValidation();
 
-
-// CARDS
-// =====
 
 const createPlaceCard = ({ name, link }) => {
   const card = new Card({ name, link }, placeCardTemplateSelector, handleCardClick);
@@ -148,41 +87,5 @@ const placeCard = new Section({
 }, placesCardListSelector);
 
 placeCard.rendererItems();
-
-
-/*export const showImage = (src, name) => {
-  placeFullImage.src = src;
-  placeFullImage.alt = name;
-  popupImageName.textContent = name;
-  openPopup(popupFullImage);
-}*/
-
-
-/*const renderPlaceCard = (item) => {
-  const cards = item.map(createPlaceCard);
-  placesCardList.append(...cards);
-}*/
-
-/*const addPlaceCard = (evt) => {
-  evt.preventDefault();
-
-  const placeCard = {
-    name: placeNameInput.value,
-    link: placeImageInput.value,
-  }
-
-  const card = new Card(placeCard, placeCardTemplateSelector)
-  placesCardList.prepend(card.createCard());
-
-  closePopup(popupAddPlaceCard);
-  formAddPlaceCardValidate.resetForm();
-}
-
-formAddPlaceCard.addEventListener('submit', addPlaceCard);*/
-
-/*buttonAddPlaceCard.addEventListener('click', () => {
-  openPopup(popupAddPlaceCard);
-});*/
-
 
 formAddPlaceCardValidate.enableValidation();
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,24 +1,13 @@
-//const popupEditProfile = document.querySelector('.popup_name_edit-profile');
 export const popupEditProfileSelector = '.popup_name_edit-profile';
-//const popupAddPlaceCard = document.querySelector('.popup_name_add-place-card');
 export const popupAddPlaceCardSelector = '.popup_name_add-place-card';
-const popupFullImage = document.querySelector('.popup_name_full-image');
-const popupImageName = popupFullImage.querySelector('.popup__image-name');
-const popups = document.querySelectorAll('.popup');
 export const popupWithImageSelector = '.popup_name_full-image';
-
-const formEditProfile = document.forms['form-edit-profile'];
-const profileNameInput = formEditProfile.querySelector('.form__input_name_name');
-const profileJobInput = formEditProfile.querySelector('.form__input_name_job');
+export const formEditProfile = document.forms['form-edit-profile'];
+export const profileNameInput = formEditProfile.querySelector('.form__input_name_name');
+export const profileJobInput = formEditProfile.querySelector('.form__input_name_job');
 export const buttonEditProfile = document.querySelector('.button-edit');
 export const buttonAddPlaceCard = document.querySelector('.button-add');
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
-
-//const placesCardList = document.querySelector('.places__grid');
+export const profileNameSelector = '.profile__name';
+export const profileJobSelector = '.profile__job';
 export const placesCardListSelector = '.places__grid';
 export const placeCardTemplateSelector = '#place-card-template';
-const formAddPlaceCard = document.forms['form-add-place-card'];
-const placeNameInput = formAddPlaceCard.querySelector('.form__input_name_name');
-const placeImageInput = formAddPlaceCard.querySelector('.form__input_name_image');
-const placeFullImage = popupFullImage.querySelector('.popup__image');
+export const formAddPlaceCard = document.forms['form-add-place-card'];
